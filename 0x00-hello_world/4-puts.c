bf6827a1c1af09458f9ef5c81e7505e0317bfd47
SeboleloAdelaide <adelaidesebolelo@gmail.com>
2023-08-14 18:02:36 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
-#include <studio.h>
-
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 { 
 	puts("Programming is like building a multilingual puzzle\n");
